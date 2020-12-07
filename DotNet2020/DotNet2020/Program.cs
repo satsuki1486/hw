2ffa6308fe6d042fb81b2c309dc903ba4dfcdb5c
zihanqin <qin@lucasware.com>
2020-12-06 22:53:51 -0500

    Presum magic

--- DotNet2020/DotNet2020/Program.cs
+++ DotNet2020/DotNet2020/Program.cs
@@ -20,28 +20,50 @@ namespace DotNet2020
             //            var time = new[] { 1, 2, 4 };
             //            var directions = new[] { 0, 1, 1 };
             // Connected Components
-            var itemAssociation1 = new List<PairString>
-            {
-                new PairString("item1", "item2"),
-                new PairString("item3", "item4"),
-                new PairString("item4", "item5")
-            };
-            var itemAssociation2 = new List<PairString>
-            {
-                new PairString("item1", "item2"),
-                new PairString("item6", "item7"),
-                new PairString("item6", "item8"),
-                new PairString("item3", "item4"),
-                new PairString("item4", "item5"),
-            };
-            var itemAssociation3 = new List<PairString>
-            {
-                new PairString("item1", "item2"),
-                new PairString("item4", "item5"),
-                new PairString("item3", "item4"),
-                new PairString("item1", "item4")
-            };
-            var result = LargestItemAssociation.Execute(itemAssociation2);
+            //            var itemAssociation1 = new List<PairString>
+            //            {
+            //                new PairString("item1", "item2"),
+            //                new PairString("item3", "item4"),
+            //                new PairString("item4", "item5")
+            //            };
+            //            var itemAssociation2 = new List<PairString>
+            //            {
+            //                new PairString("item1", "item2"),
+            //                new PairString("item6", "item7"),
+            //                new PairString("item6", "item8"),
+            //                new PairString("item3", "item4"),
+            //                new PairString("item4", "item5"),
+            //            };
+            //            var itemAssociation3 = new List<PairString>
+            //            {
+            //                new PairString("item1", "item2"),
+            //                new PairString("item4", "item5"),
+            //                new PairString("item3", "item4"),
+            //                new PairString("item1", "item4")
+            //            };
+            //            var result = LargestItemAssociation.Execute(itemAssociation2);
+            // Items in containers
+            //            var str = "|**|*|*";
+            //            var start = new[] { 1, 1 };
+            //            var end = new[] {5, 6};
+            // // Output is [2, 3]
+            //            var str = "*|*";
+            //            var start = new[] { 1 };
+            //            var end = new[] { 3 };
+            // // Output is [0]
+            //            var str = "****|*|";
+            //            var start = new[] { 1, 4, 2 };
+            //            var end = new[] { 3, 7, 7 };
+            //            // Output is [0, 1, 1]
+            //            var str = "*******";
+            //            var start = new[] { 1, 4, 2 };
+            //            var end = new[] { 3, 7, 7 };
+            //            // Output is [0, 0, 0]
+            var str = "||||||||||";
+            var start = new[] { 1, 4, 2 };
+            var end = new[] { 3, 7, 7 };
+            // Output is [0, 0, 0]
+            var result = ItemsInContainers.GetItemsInContainer(str, start, end);
         }
     }
 }
---
+++ DotNet2020/DotNet2020/Yamaxun/ItemsInContainers.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DotNet2020.Yamaxun
+{
+    public static class ItemsInContainers
+    {
+        /*
+         * start  (count the *s in between) end
+         *   !   |-------------------------| !
+         * find first | to start's right
+         * find first | to end's left
+         */
+        public static int[] GetItemsInContainer(string s, int[] startIndicies, int[] endIndicies)
+        {
+            if (string.IsNullOrWhiteSpace(s) || startIndicies == null || endIndicies == null ||
+                startIndicies.Length != endIndicies.Length)
+                return new int[] { };
+            var result = new int[startIndicies.Length];
+            // presum
+            var countOfStars = new int[s.Length];
+            // the closest '|' to position i on the left
+            var leftClosestIdx = new int[s.Length];
+            // the closest '|' to position i on the right
+            var rightClosestIdx = new int[s.Length];
+
+            var curr = -1;
+            var sum = 0;
+            for (var i = 0; i < s.Length; i++)
+            {
+                if (s[i] == '*')
+                    sum++;
+                countOfStars[i] = sum;
+            }
+
+            for (var i = 0; i < s.Length; i++)
+            {
+                if (s[i] == '|')
+                    curr = i;
+                leftClosestIdx[i] = curr;
+            }
+
+            curr = -1;
+            for (var i = s.Length - 1; i >= 0; i--)
+            {
+                if (s[i] == '|')
+                    curr = i;
+                rightClosestIdx[i] = curr;
+            }
+
+            for (var i = 0; i < startIndicies.Length; i++)
+            {
+                var start = startIndicies[i] - 1;
+                var end = endIndicies[i] - 1;
+                if (leftClosestIdx[end] != -1 && rightClosestIdx[start] != -1)
+                {
+                    result[i] = countOfStars[leftClosestIdx[end]] - countOfStars[rightClosestIdx[start]];
+                }
+            }
+
+            return result;
+        }
+    }
+}
